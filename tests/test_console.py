1f3f34babbb3db54c6499e8c92cd4ca519943245
kinyarasam <samkinyara2000@gmail.com>
2022-08-07 00:30:22 +0300

    console tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+import unittest
+import cmd
+
+
+class TestConsole(unittest.TestCase):
+    """
+    """
+    # with patch('sys.stdout', new=StringIO()) as f:
+    #     HBNBCommand().onecmd("help show")
 
 
 if __name__ == "__main__":
